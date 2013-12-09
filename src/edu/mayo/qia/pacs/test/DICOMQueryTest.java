e4bd98125a201876ffbf951a4423a3f863e6e784
Daniel Blezek <blezek.daniel@mayo.edu>
2013-12-09 13:14:11 -0600

    Remove some unused imports

--- src/edu/mayo/qia/pacs/test/DICOMQueryTest.java
+++ src/edu/mayo/qia/pacs/test/DICOMQueryTest.java
@@ -1,21 +1,18 @@
 package edu.mayo.qia.pacs.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.data.Tag;
-import org.dcm4che2.data.VR;
-import org.dcm4che2.net.NetworkConnection;
 import org.junit.Test;
 
 import edu.mayo.qia.pacs.components.Device;
 import edu.mayo.qia.pacs.components.Pool;
-import edu.mayo.qia.pacs.dicom.DcmMoveException;
 import edu.mayo.qia.pacs.dicom.DcmQR;
 import edu.mayo.qia.pacs.dicom.TagLoader;
 
