f6a40d6209b5a92c01303b7a7c30762921274a54
Daniel Blezek <daniel.blezek@gmail.com>
2014-04-25 14:46:22 -0500

    Download the study as a zip file

--- src/edu/mayo/qia/pacs/rest/StudiesEndpoint.java
+++ src/edu/mayo/qia/pacs/rest/StudiesEndpoint.java
@@ -1,15 +1,24 @@
 package edu.mayo.qia.pacs.rest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,12 +27,16 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -33,7 +46,10 @@ import org.springframework.stereotype.Component;
 
 import com.sun.jersey.spi.resource.PerRequest;
 
+import edu.mayo.qia.pacs.components.Instance;
 import edu.mayo.qia.pacs.components.PoolManager;
+import edu.mayo.qia.pacs.components.Series;
+import edu.mayo.qia.pacs.components.Study;
 
 @Scope("prototype")
 @Component
@@ -54,7 +70,7 @@ public class StudiesEndpoint {
 
   @POST
   @Produces(MediaType.APPLICATION_JSON)
-  public Response get(@Context UriInfo uriInfo) throws Exception {
+  public Response searchStudies(@Context UriInfo uriInfo, JSONObject qParams) throws Exception {
     final Set<String> columns = new HashSet<String>(Arrays.asList(new String[] { "PatientID", "PatientName", "AccessionNumber", "StudyDescription" }));
     final Set<String> directions = new HashSet<String>();
     directions.add("ASC");
@@ -66,14 +82,25 @@ public class StudiesEndpoint {
     json.put("Records", records);
     MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
 
+    logger.info("query parameters\n" + queryParameters);
+
+    logger.info("query object\n" + qParams);
+
     StringBuilder query = new StringBuilder("select * from STUDY where PoolKey = ?");
     ArrayList<Object> parameters = new ArrayList<Object>();
     parameters.add(poolKey);
 
-    if (queryParameters.containsKey("jtSorting")) {
+    for (String column : columns) {
+      if (qParams.has(column)) {
+        query.append(" and " + column + " like ? ");
+        parameters.add("%" + qParams.getString(column) + "%");
+      }
+    }
+
+    if (qParams.has("jtSorting")) {
       query.append(" ORDER BY ");
 
-      for (String clause : queryParameters.getFirst("jtSorting").split(",")) {
+      for (String clause : qParams.getString("jtSorting").split(",")) {
         String[] p = clause.split("\\s+");
         if (columns.contains(p[0])) {
           query.append(p[0]);
@@ -86,12 +113,11 @@ public class StudiesEndpoint {
 
     // jtStartIndex: Start index of records for current page.
     // jtPageSize: Count of maximum expected records.
-    if (queryParameters.containsKey("jtStartIndex") && queryParameters.containsKey("jtPageSize")) {
+    if (qParams.has("jtStartIndex") && qParams.has("jtPageSize")) {
       query.append(" OFFSET ? ROWS ");
-      parameters.add(queryParameters.getFirst("jtStartIndex"));
+      parameters.add(qParams.optInt("jtStartIndex", 0));
       query.append(" FETCH NEXT ? ROWS ONLY ");
-      parameters.add(queryParameters.getFirst("jtPageSize"));
-
+      parameters.add(qParams.optInt("jtPageSize", 50));
     }
     // Also need to return the total number of records
     json.put("TotalRecordCount", template.queryForObject("select count(*) from STUDY", Integer.class));
@@ -116,6 +142,62 @@ public class StudiesEndpoint {
     });
 
     return Response.ok(json).build();
+  }
+
+  // Get as a ZIP file
+  @GET
+  @Path("/{id: [1-9][0-9]*}/zip")
+  @Produces(MediaType.APPLICATION_OCTET_STREAM)
+  public Response getZip(@PathParam("id") int id) throws Exception {
+    Query query;
+    final String regex = "[^0-9a-zA-Z_-]";
+    Session session = sessionFactory.getCurrentSession();
+    if (!session.getTransaction().isActive()) {
+      session.getTransaction().begin();
+    }
+    query = session.createQuery("from Study where PoolKey = :poolkey and StudyKey = :id");
+    query.setInteger("poolkey", poolKey);
+    query.setInteger("id", id);
+    final Study study = (Study) query.uniqueResult();
+    if (study == null) {
+      return Response.status(Status.NOT_FOUND).build();
+    }
+    StreamingOutput stream = new StreamingOutput() {
+      @Override
+      public void write(OutputStream output) throws IOException {
+        byte[] buffer = new byte[1024];
+        ZipOutputStream zip = new ZipOutputStream(output);
+        File poolRootDir = poolManager.getContainer(poolKey).getPoolDirectory();
+        String path = study.PatientName.replaceAll(regex, "_");
+        // zip.putNextEntry(new ZipEntry(path));
+
+        String sub = study.StudyID == null ? "" : study.StudyID.replaceAll(regex, "_") + "-";
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        path = path + "/" + sub + format.format(study.StudyDate);
+        // zip.putNextEntry(new ZipEntry(path));
+        for (Series series : study.series) {
+          String seriesPath = path + "/" + series.SeriesDescription.replaceAll(regex, "_");
+          // zip.putNextEntry(new ZipEntry(seriesPath));
+          for (Instance instance : series.instances) {
+            String instancePath = seriesPath + "/" + instance.SOPInstanceUID + ".dcm";
+            zip.putNextEntry(new ZipEntry(instancePath));
+            File f = new File(poolRootDir, instance.FilePath);
+            // Read into the zip file
+            FileInputStream in = new FileInputStream(f);
+            int len;
+            while ((len = in.read(buffer)) > 0) {
+              zip.write(buffer, 0, len);
+            }
+            in.close();
+            zip.closeEntry();
+          }
+        }
+        zip.closeEntry();
+        zip.close();
+      }
+    };
+    String fn = study.PatientName.replaceAll(regex, "_") + ".zip";
+    return Response.ok(stream).header("content-disposition", "attachment; filename = " + fn).build();
   }
 
   /** Delete a Study. */
