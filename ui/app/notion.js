4c5845e2c2bc4bf797cc9786bccc33656ac79d9a
Daniel Blezek <daniel.blezek@gmail.com>
2014-04-25 14:46:37 -0500

    Searchable studies

--- ui/app/notion.js
+++ ui/app/notion.js
@@ -430,38 +430,46 @@ notionApp.controller ( 'StudyController', function($scope,$http,$timeout,$stateP
     $http.post('/rest/pool/' + $scope.pool.get('poolKey') + '/studies',
     {
       jtStartIndex: start,
-      jtPageSize: $scope.pageSize
+      jtPageSize: $scope.pageSize,
+      PatientID : $scope.PatientID,
+      PatientName : $scope.PatientName,
+      AccessionNumber : $scope.AccessionNumber
     }
-    )
-    .success(function(data,status,headers) {
-      console.log("got ", data)
-      $scope.studies = data
-      $scope.numberOfItems = data.TotalRecordCount
-    });
-  };
+  )
+  .success(function(data,status,headers) {
+    console.log("got ", data)
+    $scope.studies = data
+    $scope.numberOfItems = data.TotalRecordCount
+  });
+};
 
-    $scope.$watch('currentPage', $scope.reload);
+$scope.$watch('currentPage', $scope.reload);
+$scope.clear = function() {
+  $scope.PatientID = "";
+  $scope.PatientName = "";
+  $scope.AccessionNumber = "";
+  $scope.reload()
 
-
-    $scope.deleteStudy = function(study) {
-      $scope.study = study
-      $modal.open ({
-        templateUrl: 'partials/modal.html',
-        scope: $scope,
-        controller: function($scope, $modalInstance) {
-          $scope.title = "Delete study?"
-          $scope.message = "Delete study " + study.StudyDescription + " for " + study.PatientName + " / " + study.PatientID + " / " + study.AccessionNumber
-          $scope.ok = function(){
-            $http.delete("/rest/pool/" + $scope.pool.get("poolKey") + "/studies/" + study.StudyKey)
-            .success(function() {
-              $scope.reload()
-              $modalInstance.dismiss()
-            })
-          };
-          $scope.cancel = function() { $modalInstance.dismiss() };
-        }
-      });
-    };
+}
+$scope.deleteStudy = function(study) {
+  $scope.study = study
+  $modal.open ({
+    templateUrl: 'partials/modal.html',
+    scope: $scope,
+    controller: function($scope, $modalInstance) {
+      $scope.title = "Delete study?"
+      $scope.message = "Delete study " + study.StudyDescription + " for " + study.PatientName + " / " + study.PatientID + " / " + study.AccessionNumber
+      $scope.ok = function(){
+        $http.delete("/rest/pool/" + $scope.pool.get("poolKey") + "/studies/" + study.StudyKey)
+        .success(function() {
+          $scope.reload()
+          $modalInstance.dismiss()
+        })
+      };
+      $scope.cancel = function() { $modalInstance.dismiss() };
+    }
+  });
+};
 
 
 
