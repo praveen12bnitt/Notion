eaaa3f5ceb6b5cbbd8888b3b9d3e3d04da327db8
Daniel Blezek <daniel.blezek@gmail.com>
2014-07-03 11:08:55 -0500

    Updated documentation

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Notion: a PACS for researchers'
-copyright = u'2013, William Ryan, Patricio Fajnwaks, Daniel Blezek, Steve Langer, Brad Erickson'
+copyright = u'2014, William Ryan, Patricio Fajnwaks, Daniel Blezek, Steve Langer, Brad Erickson'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
