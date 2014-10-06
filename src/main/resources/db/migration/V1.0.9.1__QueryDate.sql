bd65124996d70701c552562ddd1cc4ac76ce74f3
Daniel Blezek <daniel.blezek@gmail.com>
2014-10-06 11:12:24 -0500

    Java and SQL changes to support review of prior queries

---
+++ src/main/resources/db/migration/V1.0.9.1__QueryDate.sql
@@ -0,0 +1,2 @@
+ALTER TABLE QUERY add column CreatedTimestamp timestamp not null with default CURRENT TIMESTAMP;
+ALTER TABLE QUERY add column LastQueryTimestamp timestamp not null with default CURRENT TIMESTAMP;
