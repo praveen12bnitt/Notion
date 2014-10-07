2d5678ae9fdf4705432fdd84832bd0c205b94f2f
Daniel Blezek <blezek.daniel@mayo.edu>
2014-10-07 07:31:00 -0500

    Remove any nulls before altering columns to be not null

--- src/main/resources/db/migration/V1.0.9.0__AnonymizationMapDefaultValues.sql
+++ src/main/resources/db/migration/V1.0.9.0__AnonymizationMapDefaultValues.sql
@@ -1,3 +1,29 @@
+update ANONYMIZATIONMAP set OriginalPatientName = '' where OriginalPatientName is null;
+update ANONYMIZATIONMAP set AnonymizedPatientName = '' where AnonymizedPatientName is null;
+
+update ANONYMIZATIONMAP set OriginalPatientID = '' where OriginalPatientID is null;
+update ANONYMIZATIONMAP set AnonymizedPatientID = '' where AnonymizedPatientID is null;
+	
+update ANONYMIZATIONMAP set OriginalAccessionNumber = '' where OriginalAccessionNumber is null;
+update ANONYMIZATIONMAP set AnonymizedAccessionNumber = '' where AnonymizedAccessionNumber is null;
+
+update ANONYMIZATIONMAP set OriginalPatientBirthDate = '' where OriginalPatientBirthDate is null;
+update ANONYMIZATIONMAP set AnonymizedPatientBirthDate = '' where AnonymizedPatientBirthDate is null;
+
+	
+update ANONYMIZATIONMAPTEMP set OriginalPatientName = '' where OriginalPatientName is null;
+update ANONYMIZATIONMAPTEMP set AnonymizedPatientName = '' where AnonymizedPatientName is null;
+
+update ANONYMIZATIONMAPTEMP set OriginalPatientID = '' where OriginalPatientID is null;
+update ANONYMIZATIONMAPTEMP set AnonymizedPatientID = '' where AnonymizedPatientID is null;
+	
+update ANONYMIZATIONMAPTEMP set OriginalAccessionNumber = '' where OriginalAccessionNumber is null;
+update ANONYMIZATIONMAPTEMP set AnonymizedAccessionNumber = '' where AnonymizedAccessionNumber is null;
+
+update ANONYMIZATIONMAPTEMP set OriginalPatientBirthDate = '' where OriginalPatientBirthDate is null;
+update ANONYMIZATIONMAPTEMP set AnonymizedPatientBirthDate = '' where AnonymizedPatientBirthDate is null;
+
+
 alter table ANONYMIZATIONMAP alter column OriginalPatientName not null;
 alter table ANONYMIZATIONMAP alter column AnonymizedPatientName not null;
 
