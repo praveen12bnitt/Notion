e8dd69ca493e68951ed046609145d35a741d032a
Daniel Blezek <daniel.blezek@gmail.com>
2014-12-17 15:39:39 -0600

    NULL is default, if a column is nullable

--- src/main/resources/db/migration/V1.1.0.1__StudyKey_to_QueryResult.sql
+++ src/main/resources/db/migration/V1.1.0.1__StudyKey_to_QueryResult.sql
@@ -1,3 +1,3 @@
 
 -- Default the StudyKey to NULL.  Indicates no study has been associated with this query
-ALTER TABLE QUERYRESULT add column StudyKey int with NULL;
+ALTER TABLE QUERYRESULT add column StudyKey int;
