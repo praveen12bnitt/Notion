981be775ee79108d0fe0950dfa40619ce4eb3d75
Daniel Blezek <daniel.blezek@gmail.com>
2015-01-26 07:45:12 -0600

    put in build

--- src/main/resources/public/dashboard/js/jqplot.js
+++ src/main/resources/public/dashboard/js/jqplot.js
@@ -1,9 +1,15 @@
 (function () {
 
+  var jqChartIdCounter = 0;
+
+  freeboard.addStyle('.jqchart', "width:100%; height:300px;");
+
+
   var jqPlotWidget = function (settings) {
 
     var self = this;
     var currentSettings = settings;
+    var chartId = "jqChart-" + jqChartIdCounter++;
     var htmlElement;
     var data;
     var options;
@@ -18,7 +24,7 @@
       $(element).append('<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/jquery.jqplot.min.css" />');
 
       //add the chart div to the dom
-      var chartDiv = '<div id="' + currentSettings.id + '" style="height:' + currentSettings.chartHeight + 'px;width:' + currentSettings.chartWidth + 'px;"></div>';
+      var chartDiv = '<div id="' + chartId + '" class="jqchart"></div>';
       console.log(chartDiv);
       htmlElement = $(chartDiv);
       $(element).append(htmlElement);
@@ -32,74 +38,59 @@
     this.onCalculatedValueChanged = function (settingName, newValue) {
       console.log('onCalculatedValueChanged for ' + settingName);
 
-      if ( settingName == 'chartWidth') {
-        $(currentSettings.id).width(newValue);
+      if (settingName == 'data') {
+        data = newValue;
       }
 
-      if (settingName == 'data')
-        data = newValue;
+      if (settingName == 'options') {
+        options = newValue;
+      }
 
-        if (settingName == 'options')
-          options = newValue;
+      //render the chart
+      htmlElement.empty();
+      $.jqplot(chartId, data, options);
+    }
 
-          //render the chart
-          htmlElement.empty();
-          $.jqplot(currentSettings.id, data, options);
-        }
+    this.onDispose = function () {
+    }
 
-        this.onDispose = function () {
-        }
+    this.getHeight = function () {
+      return Number(currentSettings.height);
+    }
 
-        this.getHeight = function () {
-          return Number(currentSettings.height);
-        }
+    this.onSettingsChanged(settings);
+  };
 
-        this.onSettingsChanged(settings);
-      };
-
-      freeboard.loadWidgetPlugin({
-        "type_name": "jqPlotWidget",
-        "display_name": "jqPlot",
-        "fill_size": true,
-        "external_scripts": [
-        "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/jquery.jqplot.min.js",
-        "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.pieRenderer.min.js",
-        "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.barRenderer.min.js",
-        "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.categoryAxisRenderer.min.js",
-        "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.pointLabels.min.js"
-        ],
-        "settings": [
-      {
-        "name": "id",
-        "display_name": "id",
-        "default_value": "chart1",
-        "description": "dom element id of the chart (must be unique for multiple charts)"
-      },
-    {
-      "name": "data",
-      "display_name": "Chart Data",
-      "type": "calculated",
-      "description": "The data to plot"
-    },
-  {
-    "name": "options",
-    "display_name": "Chart Options",
-    "type": "calculated",
-    "description": "js object containing jqPlot options for chart"
-  },
+  freeboard.loadWidgetPlugin({
+    "type_name": "jqPlotWidget",
+    "display_name": "jqPlot",
+    "fill_size": true,
+    "external_scripts": [
+    "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/jquery.jqplot.min.js",
+    "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.pieRenderer.min.js",
+    "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.barRenderer.min.js",
+    "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.categoryAxisRenderer.min.js",
+    "http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/plugins/jqplot.pointLabels.min.js"
+    ],
+    "settings": [
+{
+  "name": "data",
+  "display_name": "Chart Data",
+  "type": "calculated",
+  "description": "The data to plot"
+},
+{
+  "name": "options",
+  "display_name": "Chart Options",
+  "type": "calculated",
+  "description": "js object containing jqPlot options for chart"
+},
 {
   "name": "chartHeight",
   "display_name": "Chart Height (px)",
   "type": "number",
   "default_value": 300,
   "description": "chart height in pixels"
-},
-{
-  "name": "chartWidth",
-  "display_name": "Chart Widgth (px)",
-  "type": "number",
-  "default_value": 300,
-  "description": "chart width in pixels"
 },
 {
   "name": "height",
